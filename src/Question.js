e0db324893e4ddc54fc4308d4365d67b4f788779
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-13 20:16:53 +0700

    Displaying Questions

--- src/Question.js
+++
@@ -1,7 +0,0 @@
-function Question() {
-    return (
-        <div>Question</div>
-    )
-}
-
-export default Question;
--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
     // 'loading', 'error', 'ready', 'active', 'finished'
     status: 'loading',
+    index: 0,
 };
 
 function reducer(state, action) {
@@ -33,7 +34,7 @@ function reducer(state, action) {
     }
 }
 export default function App() {
-    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
 
@@ -56,7 +57,9 @@ export default function App() {
                         dispatch={dispatch}
                     />
                 )}
-                {status === 'active' && <Question />}
+                {status === 'active' && (
+                    <Question question={questions[index]} />
+                )}
             </Main>
         </div>
     );
