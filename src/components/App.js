10df22703c7c629c580eeb0b1352d811b6117093
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-14 19:10:25 +0700

    Handling New Answers

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
     // 'loading', 'error', 'ready', 'active', 'finished'
     status: 'loading',
     index: 0,
+    answer: null,
+    points: 0,
 };
 
 function reducer(state, action) {
@@ -29,12 +31,25 @@ function reducer(state, action) {
             };
         case 'start':
             return { ...state, status: 'active' };
+        case 'newAnswer':
+            const question = state.questions.at(state.index);
+            return {
+                ...state,
+                answer: action.payload,
+                points:
+                    action.payload === question.correctOption
+                        ? state.points + question.points
+                        : state.points,
+            };
         default:
             throw new Error('Unknown action');
     }
 }
 export default function App() {
-    const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status, index, answer }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
 
     const numQuestions = questions.length;
 
@@ -58,7 +73,11 @@ export default function App() {
                     />
                 )}
                 {status === 'active' && (
-                    <Question question={questions[index]} />
+                    <Question
+                        question={questions[index]}
+                        dispatch={dispatch}
+                        answer={answer}
+                    />
                 )}
             </Main>
         </div>
