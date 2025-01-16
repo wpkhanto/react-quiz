9ce854939e2a79aa55a2fd7383af9b389d001bb6
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-16 22:22:25 +0700

    Displaying Progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
     questions: [],
@@ -49,12 +50,16 @@ function reducer(state, action) {
     }
 }
 export default function App() {
-    const [{ questions, status, index, answer }, dispatch] = useReducer(
+    const [{ questions, status, index, answer, points }, dispatch] = useReducer(
         reducer,
         initialState
     );
 
     const numQuestions = questions.length;
+    const maxPossiblePoints = questions.reduce(
+        (prev, cur) => prev + cur.points,
+        0
+    );
 
     useEffect(() => {
         fetch('http://localhost:9000/questions')
@@ -77,6 +82,13 @@ export default function App() {
                 )}
                 {status === 'active' && (
                     <>
+                        <Progress
+                            index={index}
+                            numQuestions={numQuestions}
+                            points={points}
+                            maxPossiblePoints={maxPossiblePoints}
+                            answer={answer}
+                        />
                         <Question
                             question={questions[index]}
                             dispatch={dispatch}
