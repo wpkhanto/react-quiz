5f379a792a9d72a245bd6e1c18f46ae8190a0c63
Watcharaphon Khanto <wpkhanto@icloud.com>
2025-01-17 23:05:17 +0700

    Finish a  Quiz

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
     questions: [],
@@ -16,6 +17,7 @@ const initialState = {
     index: 0,
     answer: null,
     points: 0,
+    highscore: 0,
 };
 
 function reducer(state, action) {
@@ -45,12 +47,21 @@ function reducer(state, action) {
             };
         case 'nextQuestion':
             return { ...state, index: state.index + 1, answer: null };
+        case 'finish':
+            return {
+                ...state,
+                status: 'finished',
+                highscore:
+                    state.points > state.hightscore
+                        ? state.points
+                        : state.highscore,
+            };
         default:
             throw new Error('Unknown action');
     }
 }
 export default function App() {
-    const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
         reducer,
         initialState
     );
@@ -94,8 +105,20 @@ export default function App() {
                             dispatch={dispatch}
                             answer={answer}
                         />
-                        <NextButton dispatch={dispatch} answer={answer} />
+                        <NextButton
+                            dispatch={dispatch}
+                            answer={answer}
+                            index={index}
+                            numQuestions={numQuestions}
+                        />
                     </>
+                )}
+                {status === 'finished' && (
+                    <FinishScreen
+                        points={points}
+                        maxPossiblePoints={maxPossiblePoints}
+                        highscore={highscore}
+                    />
                 )}
             </Main>
         </div>
